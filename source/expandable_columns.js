544f90619a8bd19e05643a53951d7eda119b2d1a
David Chanin <chanindav@gmail.com>
2012-01-21 17:05:54 -0800

    basic functionality is working!

--- source/expandable_columns.js
+++ source/expandable_columns.js
@@ -1,22 +1,30 @@
 var ExpandableColumns = new Class({
 	Implements: [Options, Events],
 	options: {
-		//height: '300px',
+		//height: 300,
+		//width: 950
 		initialWidths: [], //[100, 150, etc...]
 		columnClass: 'column',
 		resizeHandleClass: 'resize',
 		minColumnWidth: 20,
-		height: null
+		snap: false
+		
 	},
 	initialize: function(container, options){
-		this.container = $(container).setStyle('position', 'relative');
+		this.container = $(container);
+		this.setOptions(options);
 		this.isContainerFull = false;
 		this.options.height = this.options.height || this.container.getSize().y + 'px';
-		this.setOptions(options);
+		this.options.width = this.options.width || this.container.getSize().x + 'px';
+		this.container.setStyles({
+			position: 'relative', 
+			width: this.options.width, 
+			height: this.options.height
+		});
 		this.columns = this.container.getChildren();
 		this.initializedColumns = [];
-		this.handles = [];
 		this.columns.each(function(column, i){ this.$initColumn(column, i) }, this);
+		this.columns.each(function(column, i){ this.$setupDragHandle(column, i) }, this);
 	},
 	$initColumn: function(column, i){
 		if (i == null) i = this.columns.length - 1;
@@ -32,8 +40,35 @@ var ExpandableColumns = new Class({
 		column.addClass(this.options.columnClass);
 		this.initializedColumns.push(column);
 		if (i != 0){
-			this.handles.push(new Element('div', {'class': this.options.resizeHandleClass}).inject(column));
-			this.$repositionLeftColumns(i)
+			this.$repositionLeftColumns(i);
+		}
+	},
+	$setupDragHandle: function(column, i){
+		if (i == null) i = this.columns.length - 1;
+		if (i > 0){
+			var handle = new Element('div', {'class': this.options.resizeHandleClass}).inject(column);
+			var leftColumn = this.columns[i - 1];
+			column.makeResizable({
+				handle: handle,
+				grid: this.options.snap,
+				invert: true,
+				modifiers: {x : 'width', y: false},
+				limit: {x: [this.options.minColumnWidth, null]},
+				onStart: function() {
+					var colSize = column.getComputedSize();
+					var expandableColspan = colSize.totalWidth + leftColumn.getComputedSize().totalWidth;
+					var paddingAndBorder = colSize.totalWidth - colSize.width;
+					this.maxDragWidth = expandableColspan - this.options.minColumnWidth - paddingAndBorder;
+					
+				}.bind(this),
+				onDrag: function() {
+					if(column.getWidth() > this.maxDragWidth) {
+						column.setStyle("width", this.maxDragWidth);
+					}
+					this.$stretchToMatch(leftColumn, column);
+				}.bind(this),
+				onComplete: this.fireEvent.bind(this, 'dragEnd')
+			});
 		}
 	},
 	$getTotalColumnWidths: function(columns){
@@ -54,15 +89,25 @@ var ExpandableColumns = new Class({
 			var idealWidth = currentWidth + columnLeftCornerX - nextElementBoundaryX;
 			if (idealWidth > this.options.minColumnWidth){
 				column.setStyle('width', idealWidth + 'px');
-			} else{
+			} else {
 				column.setStyle('width', this.options.minColumnWidth + 'px');
 				this.$repositionLeftColumns(colIndex - 1);
 			}
 		} else if (colIndex != 0) {
 			this.$repositionLeftColumns(colIndex - 1);
 		}
-		
-	},	
+	},
+	$stretchToMatch: function(leftColumn, rightColumn){
+		var leftColWidth = leftColumn.getComputedSize().width;
+		var leftColRightEdge = Number.from(leftColumn.getStyle('right'));
+		var rightColRightEdge = Number.from(rightColumn.getStyle('right'));
+		var rightColLeftEdge = rightColRightEdge + rightColumn.getComputedSize().totalWidth;
+		var newLeftColWidth = leftColWidth + leftColRightEdge - rightColLeftEdge;
+		leftColumn.setStyles({
+			right: rightColLeftEdge + 'px',
+			width: newLeftColWidth + 'px'
+		});
+	},
 	$containerWidth: function(){
 		return this.container.getComputedSize().totalWidth;
 	}
